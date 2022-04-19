d0cebe7ac8d6cc29a5cb6063444ef09b05c9525e
Leif Swenson <lmswenson@ucdavis.edu>
2022-04-19 14:02:56 -0700

    MakeWetDays added as non-interactive job

---
+++ MakeWetDays.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --cpus-per-task 12
+#SBATCH --mem 16000
+
+#SBATCH --job-name=MakeWetDays.job
+
+#SBATCH --output=/home/swenson/projects/PEX_feature_metrics/MakeWetDays.out
+
+#SBATCH --error=/home/swenson/projects/PEX_feature_metrics/MakeWetDays.err
+
+#SBATCH -p med
+
+#SBATCH --time=6:00:00
+
+source activate /home/swenson/.bashrc
+
+conda activate heatwave
+
+python3 /home/swenson/projects/PEX_feature_metrics/MakeWetDays.py > MakeWetDays.log
